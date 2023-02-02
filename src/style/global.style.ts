7c7a97b2c55aea4074dae2ae1fd1e4cabd7344df
brettl1991 <brettl1991@gmail.com>
2023-02-02 17:17:06 +0000

    add global style

---
+++ src/style/global.style.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+
+    box-sizing: border-box;
+    font-family: Arial, sans-serif;
+    font-size: 16px;
+    color: #333;
+    background-color: red;
+    margin: 0;
+    padding: 0;
+  }
+`;
