0cb2450dfa76713b1464c574ceb5e71d80c77354
brettl1991 <brettl1991@gmail.com>
2023-02-02 17:20:20 +0000

    style box component using styled components

--- src/components/box/box.style.ts
+++ src/components/box/box.style.ts
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-  color: green;
+  display: flex;
+
+  justify-content: center;
+  height: 50vh;
+
+  width: auto;
+  max-width: 900px;
+  padding: 20px 30px;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.4);
+  box-shadow: 0 10px 10px 10px rgba(0, 0, 0, 0.2);
+
+  @media screen and (max-width: 1000px) {
+    margin: auto 10px;
+  }
 `;
