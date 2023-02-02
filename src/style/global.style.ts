ed220908bb9c3f5e1b37df6235aaa421c0874cb3
brettl1991 <brettl1991@gmail.com>
2023-02-02 20:37:35 +0000

    change global style

--- src/style/global.style.ts
+++ src/style/global.style.ts
@@ -2,7 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
-
+    display: flex;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
     box-sizing: border-box;
     font-family: Arial, sans-serif;
     font-size: 16px;
