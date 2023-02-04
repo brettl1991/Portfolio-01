53cfbcf955371c40a49245ef70495b778ff726ac
brettl1991 <brettl1991@gmail.com>
2023-02-04 16:09:58 +0000

    feat: add and position profile img

---
+++ src/app/page.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ImgContainer = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 500%;
+  overflow: hidden;
+`;
+
+export const Main = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import Image from "next/image";
+import profilePic from "public/profile.png";
+import * as S from "./page.style";
+
 export default function Home() {
   return (
     <>
-      <div>Welcome Home Page</div>
+      <S.Main>
+        <h3>Welcome to my Portfolio</h3>
+        <S.ImgContainer>
+          <Image
+            src={profilePic}
+            alt="Picture of the author"
+            width={100}
+            height={100}
+          />
+        </S.ImgContainer>
+      </S.Main>
     </>
   );
 }
