b5246819e0e424475b0deca3ee3b911a023b1f11
brettl1991 <brettl1991@gmail.com>
2023-02-13 13:38:22 +0000

    add children to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Navbar />
         <Box>
           <Card />
+          {children}
         </Box>
       </body>
     </html>
